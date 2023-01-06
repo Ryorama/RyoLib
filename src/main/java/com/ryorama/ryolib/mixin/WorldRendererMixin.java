e98650f35f8bea8e05a51986f2b443cb4857e9a2
Ryan <redryma@icloud.com>
2023-01-06 11:35:35 -0500

    Stuff

--- src/main/java/com/ryorama/ryolib/mixin/WorldRendererMixin.java
+++ src/main/java/com/ryorama/ryolib/mixin/WorldRendererMixin.java
@@ -4,10 +4,7 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.ryorama.ryolib.core.client.world.ModifyWorldColor;
 import com.ryorama.ryolib.utils.graphics.ColorSettings;
 import com.ryorama.ryolib.utils.graphics.ColorUtil;
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.LightmapTextureManager;
-import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.Matrix4f;
@@ -18,14 +15,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import javax.annotation.Nullable;
+
 @Mixin(WorldRenderer.class)
 public abstract class WorldRendererMixin {
 
     @Shadow
+    @Nullable
     private ClientWorld world;
 
-    @Inject(method = "render(Lnet/minecraft/client/util/math/MatrixStack;FJZLnet/minecraft/client/render/Camera;Lnet/minecraft/client/render/GameRenderer;Lnet/minecraft/client/render/LightmapTextureManager;Lnet/minecraft/util/math/Matrix4f;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/LunarWorldView;getMoonPhase()I"))
-    private void renderSky(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, CallbackInfo ci) {
+    @Inject(method = "renderSky", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/LunarWorldView;getMoonPhase()I"))
+    private void renderSky(MatrixStack stack, Matrix4f matrix, float tickDelta, Camera camera, boolean bl, Runnable runnable, CallbackInfo ci) {
         Vec3f glColor = ColorUtil.glColor(ColorUtil.unpack(ColorSettings.tryParseColor(ModifyWorldColor.moonTextureHexColor)));
 
         RenderSystem.setShaderColor(glColor.getX(), glColor.getY(), glColor.getZ(), 1.0F - this.world.getRainGradient(tickDelta));
